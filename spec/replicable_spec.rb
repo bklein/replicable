59b1123ac737cda21f57f15b84ee44f9715e9239
Ben Klein <bk.romulox@gmail.com>
2013-10-17 00:17:15 -0400

    added rspec

---
+++ spec/replicable_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Replicable do
+
+  describe "#attr_replicable" do
+
+  end
+
+  describe "#has_many_replicable" do
+
+  end
+
+  describe "#has_one_replicable" do
+
+  end
+
+end
